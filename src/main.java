47bf21f25dc709e4194916b9d89309a8cf202eaf
agung permana <144643400+gunggpermanaa@users.noreply.github.com>
2023-12-30 16:18:52 +0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Window window = Window.getWindow();
 
+        Thread thread = new Thread(window);
+        thread.start();
+    }
+}
